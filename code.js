abda9b1ee5233017133603f95aca3f6bf68b7689
Andres Espejo Jimenez <andres.espejojimenez@yahoo.com>
2021-11-01 23:28:12 -0300

    primer commit

---
+++ code.js
@@ -0,0 +1,13 @@
+//Desafío: Crear un algoritmo JS simple
+
+let usuario = prompt("Ingrese su nombre");
+let lugarDeNacimiento = prompt("Ingrese lugar de nacimiento");
+let edad = parseInt(prompt("Ingrese edad"));
+console.log("Hola! " + usuario);
+console.log(
+    "El usuario nacio en " +
+    lugarDeNacimiento +
+    " y tiene " +
+    edad +
+    " años de edad"
+);
