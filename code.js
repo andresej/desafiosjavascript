81d4109ca953e13ba847a37b0d1576d69c9866fc
Andres Espejo Jimenez <andres.espejojimenez@yahoo.com>
2021-11-02 00:58:00 -0300

    new commit

--- code.js
+++ code.js
@@ -2,10 +2,11 @@
 
 let usuario = prompt("Ingrese su nombre");
 let lugarDeNacimiento = prompt("Ingrese lugar de nacimiento");
-let edad = parseInt(prompt("Ingrese edad"));
+let edad = prompt("Ingrese edad");
 console.log("Hola! " + usuario);
 console.log(
-    "El usuario nacio en " +
+    usuario + 
+    " nació en " +
     lugarDeNacimiento +
     " y tiene " +
     edad +
